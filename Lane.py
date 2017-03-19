c51ba3e7c92154e05db909c82f15e2e3749b74c4
padraig61091 <padraig61091@gmail.com>
2017-03-19 15:02:28 +0000

    fixed calculations for lane detection ROI needs to be adjusted as well as spec for lane to be followed

--- Lane.py
+++ Lane.py
@@ -29,47 +29,51 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     Rlines = cv2.HoughLinesP(Redges,1,np.pi/180,5,10,2)
     cv2.rectangle(image,(50,420),(200,480),(0,0,255),2)
     cv2.rectangle(image,(440,420),(590,480),(0,0,255),2)
-    
+    cv2.line(image,(320,380),(320,420),(255,0,0),2)
     try:
-        i=0
         LAVGT = 0
         for x1,y1,x2,y2 in Llines[0]:
+             
             angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
             if(angle >= 45 and angle <= 135 or angle <= -45 and angle >= -135 ):
                 cv2.line(image,(x1+50,y1+420),(x2+50,y2+420),(0,255,0),2)
-                LAVG = (x2+x1)/2
-                i=i+1
-                LAVGT = LAVGT + LAVG
-            LAVGT = LAVGT/i
+                LAVG = np.mean(Llines[0], axis=0)
+                LAVG = np.delete(LAVG,1)
+                LAVG = np.delete(LAVG,2)
+                LAVG = np.mean(LAVG)
+                
                           
     except:
-        print("error")
         RAVG = 1
         LAVG = 1
 
     try:
-        j=0
         RAVGT = 0
         for x1,y1,x2,y2 in Rlines[0]:
+
             angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
             if(angle >= 45 and angle <= 135 or angle <= -45 and angle >= -135 ):
                 cv2.line(image,(x1+440,y1+420),(x2+440,y2+420),(0,255,0),2)
-                RAVG = (x1+x2)/2
-                j=j+1
-                RAVGT = RAVGT + RAVG
-            RAVGT = RAVGT/i
-                          
-                        
+                
+                RAVG = np.mean(Rlines[0], axis=0)
+                RAVG = np.delete(RAVG,1)
+                RAVG = np.delete(RAVG,2)
+                RAVG = np.mean(RAVG)
     except:
-        print("error")
-    diff =RAVGT-LAVGT
-    cv2.putText(image,str(diff),(230,400), font, 1,(0,0,0),2)
-    if(diff <= 50 and diff >= -50):
-        cv2.putText(image,("FORWARD"),(230,440), font, 1,(0,0,0),2)
-    if(diff < -50):
-        cv2.putText(image,("TURN RIGHT"),(230,440), font, 1,(0,0,0),2)
-    if(diff > 50):
-        cv2.putText(image,("TURN LEFT"),(230,440), font, 1,(0,0,0),2)
+        RAVG = 1
+        LAVG = 1
+        
+    diff =int(LAVG)-int(RAVG)
+    print("LAVG: "+ str(int(LAVG)))
+    print("RAVG: "+ str(int(RAVG)))
+    print("DIFF: "+ str(diff))
+    cv2.line(image,(320+diff,380),(320+diff,420),(0,255,0),1)
+    #if(diff <= 50 and diff >= -50):
+        #cv2.putText(image,("FORWARD"),(230,440), font, 1,(0,0,0),2)
+    #if(diff < -50):
+        #cv2.putText(image,("TURN RIGHT"),(230,440), font, 1,(0,0,0),2)
+    #if(diff > 50):
+        #cv2.putText(image,("TURN LEFT"),(230,440), font, 1,(0,0,0),2)
     
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
