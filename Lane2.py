e62150d72f4aef042e3fedf144e394cd0bf95710
padraig61091 <padraig61091@gmail.com>
2017-04-12 18:24:54 +0000

    Lane detection finally working!! some leaving cert line equations came to the rescue :D

--- Lane2.py
+++ Lane2.py
@@ -15,8 +15,10 @@ camera.framerate = 5
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.5)
 font = cv2.FONT_HERSHEY_SIMPLEX
-Leftlist=[]
-Rightlist=[]
+Left_CAVG=[]
+Left_MAVG=[]
+Right_CAVG=[]
+Right_MAVG=[]
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
     
@@ -31,8 +33,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     Rgray = cv2.cvtColor(RROI,cv2.COLOR_BGR2GRAY)
     Redges = cv2.Canny(Rgray,100,200,apertureSize = 3)
     
-    Llines = cv2.HoughLinesP(Ledges,1,np.pi/180,10,10,2)
-    Rlines = cv2.HoughLinesP(Redges,1,np.pi/180,10,10,2)
+    Llines = cv2.HoughLinesP(Ledges,1,np.pi/180,10,12,25)
+    Rlines = cv2.HoughLinesP(Redges,1,np.pi/180,10,12,25)
     
     cv2.rectangle(image,(50,380),(200,480),(0,0,255),2)
     cv2.rectangle(image,(440,380),(590,480),(0,0,255),2)
@@ -45,17 +47,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
             if(angle >= 45 and angle <= 135 or angle <= -45 and angle >= -135 ):
                 cv2.line(image,(x1+50,y1+380),(x2+50,y2+380),(0,255,0),2)
-                LAVG = np.mean(Llines[0], axis=0)
-                LAVG = np.delete(LAVG,1)
-                LAVG = np.delete(LAVG,2)
-                LAVG = np.mean(LAVG) 
-                for x in range (0,25):
-                    Leftlist.append(LAVG)
-                if len(Leftlist) >= 24:    
-                    kf.em(Leftlist, n_iter=25)
-                    Leftlist = kf.smooth(Leftlist)
-                    print("LEFT: " +str(Leftlist))
-                    Leftlist=[]
+                LM =(float(y2-y1)/float(x2-x1))
+                LC = y1 - (LM * x1)
+                Left_CAVG.append(LC)
+                Left_MAVG.append(LM)
+                
+        LC = np.mean(Left_CAVG)
+        LM = np.mean(Left_MAVG)
+        LX = (100 - LC) / LM
+        LAVG = LX
+                
                 
     except:
         RAVG = 1
@@ -68,27 +69,30 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
             if(angle >= 45 and angle <= 135 or angle <= -45 and angle >= -135 ):
                 cv2.line(image,(x1+440,y1+380),(x2+440,y2+380),(0,255,0),2)
-                RAVG = np.mean(Rlines[0], axis=0)
-                RAVG = np.delete(RAVG,1)
-                RAVG = np.delete(RAVG,2)
-                RAVG = np.mean(RAVG)
+                RM=(float(y2-y1)/float(x2-x1))
+                RC= y1 - (RM * x1) 
+                Right_CAVG.append(RC)
+                Right_MAVG.append(RM)
+                
+        RC = np.mean(Right_CAVG)
+        RM = np.mean(Right_MAVG)
+        RX = (100 - RC) / RM
+        RAVG = RX
+               
 
                
 
     except:
         RAVG = 1
-        LAVG = 1     
+        LAVG = 1
+        
     diff =int(LAVG)-int(RAVG)
-    #print("LAVG: "+ str(int(LAVG)))
-    #print("RAVG: "+ str(int(RAVG)))
-    #print("DIFF: "+ str(diff))
+
+
     cv2.line(image,(320+diff,380),(320+diff,420),(0,255,0),1)
     cv2.putText(image,str(int(LAVG)),(100,360), font, 1,(0,255,0),2)
     cv2.putText(image,str(int(RAVG)),(500,360), font, 1,(0,255,0),2)
     cv2.putText(image,str(diff),(280,370), font, 1,(0,255,255),2)
-    Ledges = np.fliplr(Ledges)
-    #cv2.imshow("L", Ledges)
-    #cv2.imshow("R", Redges)
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
 
