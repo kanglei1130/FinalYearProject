d9113029fdca58be43e8afbf3e8d5c87d22bdde9
padraig61091 <padraig61091@gmail.com>
2017-04-26 22:11:58 +0000

    Added calculations to get distance to object im CM

--- camera.py
+++ camera.py
@@ -7,14 +7,13 @@ import sys
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.crop = (0,0,0,0)
-camera.framerate = 60
 rawCapture = PiRGBArray(camera, size=(640, 480))
 #camera.color_effects = (128,128)
 
-LEFT_cascade = cv2.CascadeClassifier('LEFT_CASCADE.xml')
-RIGHT_cascade = cv2.CascadeClassifier('RIGHT_CASCADE.xml')
-STOP_cascade = cv2.CascadeClassifier('STOP_CASCADE.xml')
-TRAFFIC_cascade = cv2.CascadeClassifier('TRAFFIC_CASCADE_3_STAGES.xml')
+LEFT_cascade = cv2.CascadeClassifier('Left_3.xml')
+RIGHT_cascade = cv2.CascadeClassifier('Right3.xml')
+STOP_cascade = cv2.CascadeClassifier('Traffic_3.xml')
+TRAFFIC_cascade = cv2.CascadeClassifier('Traffic_3.xml')
 
 time.sleep(0.5)
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -29,19 +28,22 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     TRAFFIC = TRAFFIC_cascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=10)
 
     for (x,y,w,h) in LEFT:
-        cv2.putText(image,'LEFT',(x,y-10), font, 1,(0,255,0))
+        dist = (((3.04*71.5*480)/(h*2.76))-55)/10
+        cv2.putText(image,'LEFT: '+str(int(dist))+"cm",(x,y+h+25), font, 1,(0,255,0))
         cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0),2)
     
     for (x,y,w,h) in RIGHT:
-        cv2.putText(image,'RIGHT',(x,y-10), font, 1,(255,0,0))
+        dist = (((3.04*71.5*480)/(h*2.76))-55)/10
+        cv2.putText(image,'RIGHT: '+str(int(dist))+"cm",(x,y+h+25), font, 1,(255,0,0))
         cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0),2)
         
-    for (x,y,w,h) in STOP:
-        cv2.putText(image,'STOP',(x,y-10), font, 1,(0,0,255))
-        cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255),2)
+##    for (x,y,w,h) in STOP:
+##        cv2.putText(image,'STOP: ' + str((w+h)/2),(x,y+h+25), font, 1,(0,0,255))
+##        cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255),2)
 
     for (x,y,w,h) in TRAFFIC:
-        cv2.putText(image,'TRAFFIC',(x,y-10), font, 1,(0,255,255))
+        dist = (((3.04*58*480)/(h*2.76))-50)/10
+        cv2.putText(image,'TRAFFIC: ' +str(int(dist))+"cm",(x,y+h+25), font, 1,(0,255,255))
         cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,255),2)
 
 
