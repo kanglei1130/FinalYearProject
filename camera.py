d574ad81541fa068344095a1ab7fd7f66edf7305
padraig61091 <padraig61091@gmail.com>
2017-04-27 10:32:37 +0000

    added small offsets for distance measurments

--- camera.py
+++ camera.py
@@ -12,7 +12,7 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 LEFT_cascade = cv2.CascadeClassifier('Left_3.xml')
 RIGHT_cascade = cv2.CascadeClassifier('Right3.xml')
-STOP_cascade = cv2.CascadeClassifier('Traffic_3.xml')
+STOP_cascade = cv2.CascadeClassifier('Stop_3.xml')
 TRAFFIC_cascade = cv2.CascadeClassifier('Traffic_3.xml')
 
 time.sleep(0.5)
@@ -22,27 +22,35 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     image = frame.array
     image = image[80:440,0:640]
+##    cv2.normalize(image,image,0,255,cv2.NORM_MINMAX)
     STOP = STOP_cascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=10)    
     LEFT = LEFT_cascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=10)
     RIGHT = RIGHT_cascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=10)
-    TRAFFIC = TRAFFIC_cascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=10)
+    TRAFFIC = TRAFFIC_cascade.detectMultiScale(image,scaleFactor=1.05,minNeighbors=10)
 
     for (x,y,w,h) in LEFT:
-        dist = (((3.04*71.5*480)/(h*2.76))-55)/10
+        h=h*.95
+        h=int(h)
+        dist = (((3.04*71.5*480)/(h*2.76))-55)
         cv2.putText(image,'LEFT: '+str(int(dist))+"cm",(x,y+h+25), font, 1,(0,255,0))
         cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0),2)
     
     for (x,y,w,h) in RIGHT:
-        dist = (((3.04*71.5*480)/(h*2.76))-55)/10
+        dist = (((3.04*70.5*480)/(h*2.76))-55)
         cv2.putText(image,'RIGHT: '+str(int(dist))+"cm",(x,y+h+25), font, 1,(255,0,0))
         cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0),2)
         
-##    for (x,y,w,h) in STOP:
-##        cv2.putText(image,'STOP: ' + str((w+h)/2),(x,y+h+25), font, 1,(0,0,255))
-##        cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255),2)
+    for (x,y,w,h) in STOP:
+        dist = (((3.04*65*480)/(h*2.76))-55)
+        cv2.putText(image,'STOP: ' +str(int(dist))+"cm",(x,y+h+25), font, 1,(0,0,255))
+        cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255),2)
 
     for (x,y,w,h) in TRAFFIC:
-        dist = (((3.04*58*480)/(h*2.76))-50)/10
+        h=h*1.08
+        h=int(h)
+        y=y-((h*1.08)-h)
+        y=int(y)
+        dist = (((3.04*61*480)/(h*2.76))-55)
         cv2.putText(image,'TRAFFIC: ' +str(int(dist))+"cm",(x,y+h+25), font, 1,(0,255,255))
         cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,255),2)
 
